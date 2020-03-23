9929ab11224f9cb1357b464055bc4a7aaf4219dc
Helsiel <62539000+Helsiel@users.noreply.github.com>
2020-03-23 10:47:12 +0200

    Version1.0.0

---
+++ Assets/Load.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour
+{
+    public static int SceneNumber;
+
+    void Start()
+    {
+        if(SceneNumber == 0)
+        {
+            StartCoroutine(ToSplashTwo());
+            }
+     }
+    IEnumerator ToSplashTwo()
+    {
+        yield return new WaitForSeconds(4);
+        SceneNumber = 1;
+        SceneManager.LoadScene(1);
+}
+
+}
---
+++ Assets/LoadMap1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadMap1 : MonoBehaviour
+{
+    public string sceneName;
+    public Button playButton;
+
+    void Start()
+    {
+        playButton.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
