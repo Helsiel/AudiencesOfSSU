994f02cff6ffbe49eb137767267840befd418264
Helsiel <62539000+Helsiel@users.noreply.github.com>
2020-03-23 14:24:10 +0200

    Vesion 1.0.5

---
+++ Assets/AddLeftMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AddLeftMenu : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    public GameObject MenuNavigation;
+    public bool IfPause=false;
+
+    
+    void Update()
+    {
+        if (IfPause)
+        {
+            Pause();
+        }
+        else if (!IfPause)
+        {
+            Resume();
+        }
+    }
+
+    public void Pause()
+    {
+        PauseMenu.SetActive(false);
+        MenuNavigation.SetActive(true);
+        IfPause = true;
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(true);
+        MenuNavigation.SetActive(false);
+        IfPause = false;
+        Time.timeScale = 1f;
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+   
+
+}
